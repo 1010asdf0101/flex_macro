9ef9e3959d1c0e10687b656f04b6b17da4096636
1010asdf0101 <shawnman1999@gmail.com>
2023-06-22 11:54:55 +0900

    windows

--- windows10/build.ps1
+++ windows10/build.ps1
@@ -1,8 +1,7 @@
 pyinstaller -F leave.py
 pyinstaller -F on.py
-Move-item -Path .\dist\leave -Destination ~\leave
-Move-item -Path .\dist\on -Destination "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
-Set-alias byebye "~/leave $args[1] $args[2] $args[3]"
-
-rm -rf build/ dist/
-rm *.spec
+Move-item -Path .\dist\leave.exe -Destination ~\leave.exe
+Move-item -Path .\dist\on.exe -Destination "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\on.exe"
+Remove-Item .\build\
+Remove-Item .\dist\
+Remove-Item *.spec
--- windows10/win_leave.py
+++ windows10/win_leave.py
@@ -1,7 +1,6 @@
 ###############################################################
 # Source Code originated from https://choiblog.tistory.com/55 #
 ###############################################################
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -14,17 +13,17 @@ import os
 import sys
 
 # get arguments from command line
-my_id = sys.argv[1]
-my_pw = sys.argv[2]
-shutdown_wait_time = sys.argv[3]
+my_id = 'your_id'
+my_pw = 'your_pw'
+shutdown_wait_time = 5
 
 # Browser Options
 options = Options()
-options.add_argument('--incognito') # inprivate mode
+options.add_argument('--incognito') #inprivate mode
 # width of window size should be larger than certain value for access time_btn
 options.add_argument('--window-size=1600,1080')
-#options.add_experimental_option('detach', True) # terminal chorme when process is terminated
-options.add_experimental_option("excludeSwitches", ['enable-logging']) # disable unused messages
+options.add_experimental_option('detach', True) # terminal chorme when process is terminated
+options.add_experimental_option("excludeSwitches", ['enable-logging']) # 불필요한 메시지 제거
 
 service = Service(ChromeDriverManager().install())
 
@@ -32,24 +31,24 @@ driver = webdriver.Chrome(service=service, options=options)
 wait = WebDriverWait(driver, 10)
 driver.get(r"https://flex.team/auth/login?nextUrl=%2Fhome")
 driver.implicitly_wait(time_to_wait=10)
-# ID
 driver.find_element(By.XPATH, \
     r"/html/body/div[1]/main/div/div/div/article/div/div[2]/div/div/form/div/label/div/div[1]/div[2]/div[2]/input")\
         .send_keys(my_id)
 driver.find_element(By.XPATH, \
     r"/html/body/div[1]/main/div/div/div/article/div/div[2]/div/div/form/div/label/div/div[2]/button")\
         .click()
-# PW
 driver.find_element(By.XPATH, \
     r"/html/body/div[1]/main/div/div/div/article/div/div[2]/form/div/div[2]/label/div/div[1]/div[2]/div[2]/input")\
         .send_keys(my_pw)
-driver.find_element(By.XPATH,\
-    r"/html/body/div[1]/main/div/div/div/article/div/div[2]/form/div/button[1]")\
-        .click()
-# waiting for main page of flex
+driver.find_element(By.XPATH, r"/html/body/div[1]/main/div/div/div/article/div/div[2]/form/div/button[1]").click()
+# site login
 sleep(5)
-time_btn = wait.until(EC.element_to_be_clickable((By.XPATH, \
-    r"/html/body/div[1]/div/div/div/nav/div/div[1]/div[2]/div/div/button")))
+time_btn = wait.until(\
+    EC.element_to_be_clickable((By.XPATH, \
+        r"/html/body/div[1]/div/div/div/nav/div/div[1]/div[2]/div/div/button")))
 time_btn.click()
-driver.find_element(By.XPATH, r"/html/body/div[6]/div/div/div/div/div/div[3]/div[2]/div").click()
+driver.find_element(By.XPATH, r"/html/body/div[6]/div/div/div/div/div/div[4]/div[2]").click()
+confirm_btn = wait.until(EC.element_to_be_clickable((By.XPATH, r"/html/body/div[6]/div/div[2]/div/div[2]/div[3]/div/div[2]/button[2]")))
+confirm_btn.click()
 sleep(max(2, int(shutdown_wait_time)))
+os.system("shutdown now -f")
